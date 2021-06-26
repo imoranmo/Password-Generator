68c65e3d718d3234052b504d74b32f5ef2ff044d
imoranmo <imoranmo@gmail.com>
2021-06-25 21:29:30 -0700

    Adding window commands

--- script.js
+++ script.js
@@ -2,7 +2,35 @@
 var generateBtn = document.querySelector("#generate");
 
 
+var characters = {
 
+  characterlength: function() {
+     var length = window.prompt ("How long would you like your password?");
+
+      if (length < 8 || length > 128) {
+          length = characters.characterlength();
+        }
+     return length;
+     },
+ lowercase: function() {
+       var lower = window.confirm ("Would you like to include lower case characters?");
+
+      return lower
+ },
+ uppercase: function() {
+      var upper = window.confirm ("Would you like to include upper case characters?");
+
+      return upper
+ },
+ symbols: function() {
+      var symbol = window.confirm ("Would you like to include symbols?");
+      return symbol;
+
+ }
+
+  }
+
+console.log(characters.symbols());
 
 function getLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -28,6 +56,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  function generatePassword () {}
 
 }
 
