771b18fc47e911c744eef147ec0e98bf240bb5be
imoranmo <imoranmo@gmail.com>
2021-06-25 18:09:45 -0700

    Creating character functions

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+function getLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getSymbol() {
+	var symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
